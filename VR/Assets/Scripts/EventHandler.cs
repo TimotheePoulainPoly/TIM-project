56e6667f20f068e1332638d777439677aecaedf6
Timothée Poulain <54988228+TimotheePoulainPoly@users.noreply.github.com>
2023-01-21 15:04:53 +0100

    VR : Object picking

--- VR/Assets/Scripts/EventHandler.cs
+++ VR/Assets/Scripts/EventHandler.cs
@@ -5,6 +5,10 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class EventHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject itemList;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
     public void OnHoverEntered(HoverEnterEventArgs args)
     {
         Debug.Log($"{args.interactorObject} hovered over " + this.gameObject.name);
@@ -15,5 +19,19 @@ public class EventHandler : MonoBehaviour
     {
         Debug.Log($"{args.interactorObject} stopped hovering over " + this.gameObject.name);
         GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+    }
+
+    public void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        Debug.Log($"{args.interactorObject} grabbed " + this.gameObject.name);
+        initialPosition = this.transform.position;
+        initialRotation = this.transform.rotation;
+    }
+ 
+    public void OnSelectExited(SelectExitEventArgs args)
+    {
+        Debug.Log($"{args.interactorObject} stopped grabbing " + this.gameObject.name);
+        this.transform.position = initialPosition;
+        this.transform.rotation = initialRotation;
     }
 }
