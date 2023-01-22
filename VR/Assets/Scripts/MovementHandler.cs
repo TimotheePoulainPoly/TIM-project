0acea9a9bfca8d0d7a4c29242a5e72cb827d7365
SofianeBa <sofianebabouri9@gmail.com>
2023-01-22 13:02:47 +0100

    Test 2

--- VR/Assets/Scripts/MovementHandler.cs
+++ VR/Assets/Scripts/MovementHandler.cs
@@ -1,30 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MovementHandler : MonoBehaviour
 {
     public float speed = 5.0f;
-    public OVRInput.Controller controller;
+    XRController controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<XRController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Récupérer les inputs du joystick droit
-        float horizontal = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick, controller).x;
-        float vertical = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick, controller).y;
+        float horizontal = controller.inputDevice.GetAxis(CommonUsages.secondary2DAxisX);
+        float vertical = controller.inputDevice.GetAxis(CommonUsages.secondary2DAxisY);
 
-        // Vérifier si la manette droite est suivie
-        if(OVRInput.GetControllerPositionTracked(controller))
-        {
-            // Appliquer les inputs pour déplacer la caméra ou le joueur
-            transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
-        }
+        // Appliquer les inputs pour déplacer la caméra ou le joueur
+        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
     }
 }
