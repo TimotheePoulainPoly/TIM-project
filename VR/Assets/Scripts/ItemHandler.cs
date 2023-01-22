07722bef9e374cf9f275bb54b003d29cc5f2ddba
Timothée Poulain <54988228+TimotheePoulainPoly@users.noreply.github.com>
2023-01-22 12:07:30 +0100

    VR : New room

--- VR/Assets/Scripts/ItemHandler.cs
+++ VR/Assets/Scripts/ItemHandler.cs
@@ -27,4 +27,14 @@ public class ItemHandler : MonoBehaviour
         GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
         realItem.GetComponent<EventHandler>().UngrabbedFromList();
     }
+
+    public void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        GetComponent<Renderer>().material.color = new Color32(197, 198, 160, 255);
+    }
+
+    public void OnHoverExited(HoverExitEventArgs args)
+    {
+        GetComponent<Renderer>().material.color = new Color32(185, 185, 185, 255);
+    }
 }
